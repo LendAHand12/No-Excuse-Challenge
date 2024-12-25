6f35ead72381787bf886aac0b3e414ff5e155f66
LendAHand12 <lendahand8888@gmail.com>
2024-12-25 13:20:51 +0700

    ok

--- client/src/constants/transStatus.ts
+++ client/src/constants/transStatus.ts
@@ -1,8 +1,10 @@
 export default [
-  { status: "REGISTER", color: "bg-green-600" },
-  { status: "DIRECT", color: "bg-green-600" },
-  { status: "REFERRAL", color: "bg-green-600" },
-  { status: "FINE", color: "bg-green-600" },
-  { status: "DIRECTHOLD", color: "bg-yellow-600" },
-  { status: "REFERRALHOLD", color: "bg-yellow-600" },
+  { status: 'REGISTER', color: 'bg-green-600' },
+  { status: 'DIRECT', color: 'bg-green-600' },
+  { status: 'PIG', color: 'bg-pink-600' },
+  { status: 'COMPANY', color: 'bg-blue-600' },
+  { status: 'REFERRAL', color: 'bg-green-600' },
+  { status: 'FINE', color: 'bg-green-600' },
+  { status: 'DIRECTHOLD', color: 'bg-yellow-600' },
+  { status: 'REFERRALHOLD', color: 'bg-yellow-600' },
 ];
