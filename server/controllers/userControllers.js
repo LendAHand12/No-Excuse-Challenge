ac36301428ace887c560af1a4135f2b195e99636
LendAHand12 <lendahand8888@gmail.com>
2025-02-18 16:02:06 +0700

    fix ref

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -872,9 +872,11 @@ const getListChildOfUser = asyncHandler(async (req, res) => {
   let result = [];
 
   const parent = await Tree.findOne({ userId: req.user.id }).lean();
-  if (!parent || parent.children.length <= 5) return [];
-
-  result = await getAllDescendants(req.user.id);
+  if (!parent || parent.children.length <= 5) {
+    result = [];
+  } else {
+    result = await getAllDescendants(req.user.id);
+  }
 
   res.json(result);
 });
