13e2622e7bab0207d88ef3ea563822a87269eea8
LendAHand12 <lendahand8888@gmail.com>
2025-05-03 18:50:34 +0700

    fix pagination

--- client/src/pages/Admin/Withdraw/index.jsx
+++ client/src/pages/Admin/Withdraw/index.jsx
@@ -12,6 +12,7 @@ import Admin from '@/api/Admin';
 import { shortenWalletAddress } from '@/utils';
 import { transfer } from '@/utils/smartContract';
 import { useSelector } from 'react-redux';
+import CustomPagination from '@/components/CustomPagination';
 
 const AdminWithdrawPages = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -480,76 +481,11 @@ const AdminWithdrawPages = () => {
         )}
         {!loading && data.length === 0 && <NoContent />}
         {!loading && data.length > 0 && (
-          <nav
-            className="flex items-center justify-between pt-4"
-            aria-label="Table navigation"
-          >
-            <span className="text-sm font-normal text-gray-500">
-              Showing{' '}
-              <span className="font-semibold text-gray-900">
-                {objectFilter.pageNumber}
-              </span>{' '}
-              of{' '}
-              <span className="font-semibold text-gray-900">{totalPage}</span>{' '}
-              page
-            </span>
-            <ul className="inline-flex items-center -space-x-px">
-              <li>
-                <button
-                  disabled={objectFilter.pageNumber === 1}
-                  onClick={() =>
-                    handleChangePage(parseInt(objectFilter.pageNumber) - 1)
-                  }
-                  className={`block px-3 py-2 ml-0 leading-tight text-gray-500 ${
-                    objectFilter.pageNumber === 1 ? 'bg-gray-100' : 'bg-white'
-                  } border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700`}
-                >
-                  <span className="sr-only">Previous</span>
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
-              </li>
-              <li>
-                <button
-                  disabled={objectFilter.pageNumber === totalPage}
-                  onClick={() =>
-                    handleChangePage(parseInt(objectFilter.pageNumber) + 1)
-                  }
-                  className={`block px-3 py-2 leading-tight text-gray-500 ${
-                    objectFilter.pageNumber === totalPage
-                      ? 'bg-gray-100'
-                      : 'bg-white'
-                  } border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700`}
-                >
-                  <span className="sr-only">Next</span>
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
-              </li>
-            </ul>
-          </nav>
+          <CustomPagination
+            currentPage={parseInt(objectFilter.pageNumber)}
+            totalPages={totalPage}
+            onPageChange={handleChangePage}
+          />
         )}
       </div>
     </DefaultLayout>
