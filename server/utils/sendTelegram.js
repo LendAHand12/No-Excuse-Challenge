ebde4588ef8050ac39bc1db6de34396dedf04494
AnhKiet1010 <letrananhkiet1010@gmail.com>
2025-05-08 01:54:43 +0700

    remove error

--- server/utils/sendTelegram.js
+++ server/utils/sendTelegram.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import moment from "moment";
-import https from "https";
-
 
 const TELEGRAM_BOT_TOKEN = process.env.TELE_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELE_CHAT_ID;
@@ -19,30 +17,13 @@ export const sendTelegramMessage = async ({ userName }) => {
       <b>Chi tiết:</b> <a href="${process.env.FRONTEND_BASE_URL}/admin/withdraw">Xem chi tiết</a>
       `;
   try {
-    const agent = new https.Agent({
-      rejectUnauthorized: false, // thử bỏ verify TLS (debug)
-      keepAlive: true,           // giữ kết nối
-      timeout: 5000              // set timeout rõ ràng
-    });
-
-    const response = await axios.post(url, {
+    await axios.post(url, {
       chat_id: TELEGRAM_CHAT_ID,
       text: message,
       parse_mode: "HTML", // Markdown
-    }, { httpsAgent: agent });
-    console.log("✅ Message sent to Telegram", response.data);
+    });
+    console.log("✅ Message sent to Telegram");
   } catch (error) {
-    if (error.response) {
-      // Lỗi trả về từ server (Telegram API)
-      console.error("❌ Telegram API error:");
-      console.error("Status:", error.response.status);
-      console.error("Data:", error.response.data);
-    } else if (error.request) {
-      // Request đã gửi nhưng không nhận được response
-      console.error("❌ No response from Telegram API. Request:", error.request);
-    } else {
-      // Lỗi khác (có thể là lỗi cấu hình)
-      console.error("❌ Unknown error:", error.message);
-    }
+    console.error("❌ Error sending message:", error);
   }
 };
